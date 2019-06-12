95d7ab5db2aefa8b35f49d48c83eab856aa4a5ae
bigboy32 <34939884+bigboy32@users.noreply.github.com>
2019-06-12 15:34:22 +0200

    Start

---
+++ venv/__frames__.py
@@ -0,0 +1,10 @@
+from Abmelden import Toplevel1 as t1
+from Anmelden import Toplevel1 as t2
+from Sonstiges import Toplevel1 as t3
+from functions import raise_frame
+
+def Hvar1():
+    raise_frame(t1)
+def Hvar2():
+    raise_frame(t2)
+
